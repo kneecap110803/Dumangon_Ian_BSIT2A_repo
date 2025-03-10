ed1629753c337df46e8854baa7bf9d7777a9bb34
Ianmigu11 <idumangon@filamer.edu.ph>
2025-03-10 02:08:17 -0700

    battleship v1

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -1,1 +1,41 @@
-console.log("Battleship Game");
+// VARIABLES DECLARATION
+
+var location1 = 3;
+var locatiion2 = 4;
+var location3 = 5;
+
+var guess;
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;
+
+//game logic
+
+//while loop starting point
+while (isSunk == false) {
+    guess = prompt("Ready to shoot! (Enter Number 0-6");
+    // (|| = or)
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!")
+    } else {
+        guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+            alert("hits!")
+
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!!!")
+            }
+        }
+    }
+
+}
+
+//end of the loop
+
+var stats = "You sank the battleship at: " + guesses + "guesses" + "with the accuracy of: " + (3/guesses);
+
+alert(stats);
